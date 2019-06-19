11f32b095fec7d1d42a5c981d7baaa0e01bcdce1
Alan Smith <arsmithjr@gmail.com>
2019-06-19 12:20:27 -0400

    updated index.js to hide db credentials; created a .env file to globalize some variables (i.e., port

--- server/index.js
+++ server/index.js
@@ -1,28 +1,29 @@
 'use strict';
 
+require('dotenv').config()
 const express = require('express');
 const router = require('./router');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-
+const port = process.env.PORT;
 const app = express();
 
 app.set('view engine', 'ejs');
 
 // connect db
 const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database:'fs1030_group_project'
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DATABASE
 })
 
 // Connect to database
 db.connect(function(error){
     if(!!error){
-        console.log('Error connecting DB');
+        console.log(`Error connecting to ${process.env.DATABASE}!`);
     } else {
-    console.log('Connected');
+        console.log(`Connected to database ${process.env.DATABASE}!`);
     }
     });
 global.db = db;
@@ -35,7 +36,7 @@ app.use('/static', express.static('static'));
 
 app.use(router);
 
-app.listen(3000, () => {
-    console.log('Server is Running!');
+app.listen(port, () => {
+    console.log(`Server running on port: http://localhost:${process.env.PORT}`);
 
 });
